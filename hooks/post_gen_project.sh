6b34c2c94916b8bfae2adccbab9243494fe42674
Roy Wright <roy@wright.org>
2024-08-02 21:37:32 -0500

    correct initial state to be using hatch build system.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,6 +4,7 @@ rm -rf .idea
 rm -f metrics/*
 rm -rf tests/__pycache__
 
-rm -f taskfiles/front-end.yaml
+rm -f taskfiles/front-end.yaml taskfiles/front-end-vars.yaml
 ln -s {{ cookiecutter.build_backend }}.yaml taskfiles/front-end.yaml
+ln -s {{ cookiecutter.build_backend }}-vars.yaml taskfiles/front-end-vars.yaml
 exit 0
